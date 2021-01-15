5ec2ff75e144380c7c136b79a0dfaba9a02b0877
Amanda Carpentieri <carpentieri.a@gmail.com>
2021-01-15 11:35:28 -0800

    edits

--- src/client/Detail.tsx
+++ src/client/Detail.tsx
@@ -9,8 +9,22 @@ export default class Detail extends React.Component<IDetailProps, IDetailState>{
         }
     }
 
-    render() {
-        return <h1>{this.state.name}</h1>
+    updateName = (e: React.ChangeEvent<HTMLInputElement>) => {
+       this.setState({name: e.target.value})
+    }
+
+    render(){
+        return(
+            <section>
+                <h1>{this.state.name}</h1>
+                <div className="form-group">
+                    <label htmlFor="name">Update Name</label>
+                    <input type="text" className="form-control" defaultValue={this.state.name}
+                    onChange={this.updateName}
+                    />
+                </div>
+            </section>
+        )
     }
 
 }
